ab3a0ea7f33ec78fb6802abeda1398a043eedebd
mateus <mateusangeljr@mail.com>
2023-05-22 14:17:34 -0300

    cvti

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const btn = document.querySelectorAll('button');
+btn.forEach((item) => {
+  item.addEventListener('click', () => {
+    alert('em breve!');
+  });
+});
+
+console.log('hello world')
