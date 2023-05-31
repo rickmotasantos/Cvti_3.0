9664ba4cb8391f72b4fa9e769183d3629246bbdf
rickmotasantos <102193802+rickmotasantos@users.noreply.github.com>
2023-05-31 00:24:55 -0300

    up.
    alteracoes na responsividade do cabecalho

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,21 @@ btn.forEach((item) => {
   });
 });
 
-console.log('hello world')
+console.log('hello world');
+
+
+let btnMenu = document.querySelectorAll('.hamburguer');
+let nave = document.getElementsByTagName("nav")
+
+console.log(nave)
+
+console.log(btnMenu)
+
+btnMenu[0].addEventListener('click', ()=> {
+
+  if(nave[0].classList.contains("aparecer")){
+    nave[0].classList.remove("aparecer")
+  }else{
+    nave[0].classList.add("aparecer")
+  }
+}) 
